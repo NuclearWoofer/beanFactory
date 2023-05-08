715b1ea6f2334a2e2d24e68b54a31766e4b9c300
Michael Lopez <malopez1@email.neit.edu>
2023-05-08 13:50:49 -0400

    added upload form with validation

---
+++ src/comps/UploadForm.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+
+const UploadForm = () => {
+    //creating an error for a null image
+    const[error, setError] = useState(null);
+
+    //setting the file data
+    const [file,setFile] = useState(null);
+
+    //setting our array for allowed image types
+    const types=['image/png','image/jpeg']
+
+    //onChange (or when a file is staged for posting) we gather the image data and check that is an allowed image type
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+        //checking for image and taking the selected image data
+        if(selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('')
+        } else {
+            setFile(null);
+            setError('Please select an image file. (png or jpeg.)')
+
+        }
+    }
+    return (
+        <form>
+            <input type="file" onChange={changeHandler} />
+            <div className="output">
+                {error && <div className="error"> {error}</div> }
+                {file && <div>{file.name}</div> }
+            </div>
+        </form>
+    )
+}
+
+export default UploadForm;
