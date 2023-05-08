921502a61457dac650614c101ac9f2f5d48baf4f
Michael Lopez <malopez1@email.neit.edu>
2023-05-08 13:25:28 -0400

    adding personal touches

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>beanFactory</h1>
+      <h2>beanFactory</h2>
+      <p>the personal website of miniature hobbyist Michael Lopez</p>
     </div>
   )
 }
